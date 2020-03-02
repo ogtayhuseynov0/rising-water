986b0d68a3d07634e8aa2978f837a708b5c6c8dd
Ogtay Huseynov <ogtayhuseynov0@gmail.com>
2020-03-02 16:45:54 +0400

    asd

--- src/boot/g-map-boot.js
+++
@@ -1,8 +0,0 @@
-import GoogleMapsLoader from 'google-maps'
-
-export default ({ app, router, Vue }) => {
-  Vue.prototype.$gmaps = GoogleMapsLoader
-  Vue.prototype.$gmaps.KEY = ''
-  Vue.prototype.$gmaps.LIBRARIES = ['places', 'visualization']
-  GoogleMapsLoader.VERSION = '3.38'
-}
